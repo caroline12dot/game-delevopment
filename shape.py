63f67f211aa049c9963aa69f528173c933b1f18f
caroline12dot <ruvarasheganyeka539@gmail.com>
2026-02-20 16:48:40 +0000

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,20 @@
+import pgzrun
+import random
+WIDTH=600
+HEIGHT=600
+
+def draw():
+    screen.fill("black")
+    w=200
+    h=300
+    for i in range(15):
+     rect1=Rect((0,0),(w,h))
+     r=random.randint(0,255)
+     g=random.randint(0,255)
+     b=0
+     rect1.center=(300,300)
+     screen.draw.rect(rect1,(r,g,b))
+     w+=10
+     h-=10
+
+pgzrun.go()
